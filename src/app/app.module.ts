72a177b034a49c8b2b1d6bac2ddc8dcf78c3ba25
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-11-17 14:12:37 -0300

    [feat] primeiro componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ParentDataComponent } from './components/parent-data/parent-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ParentDataComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/parent-data/parent-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'parent-data',
+  templateUrl: './parent-data.component.html',
+  styleUrl: './parent-data.component.css'
+})
+export class ParentDataComponent {
+
+}
