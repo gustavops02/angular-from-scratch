71e459893e2fbfc621329eb5a9c2a5ba828ef381
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-11-19 18:33:55 -0300

    [feat] trabalhando com interfaces

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Animal } from '../../interfaces/Animal';
 
 @Component({
   selector: 'app-list-render',
@@ -7,11 +8,22 @@ import { Component } from '@angular/core';
 })
 export class ListRenderComponent {
 
-  animals = [
-    { name: "Turca", type: 'Dog' },
-    { name: "Tom", type: "Cat" },
-    { name: "Frida", type: "Dog" },
-    { name: "Bob", type: "Horse" }
+  animals:Animal[] = [
+    { name: "Turca", type: 'Dog', age: 8 },
+    { name: "Tom", type: "Cat", age: 4 },
+    { name: "Frida", type: "Dog", age: 5 },
+    { name: "Bob", type: "Horse", age: 1 }
   ]
+
+animalDetails = '';
+
+  showAge(animal: Animal) {
+    if(animal.age === 1) {
+      this.animalDetails = `O pet ${animal.name} tem ${animal.age} ano. `
+
+    } else {
+      this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos. `
+    }
+  }
   
 }
