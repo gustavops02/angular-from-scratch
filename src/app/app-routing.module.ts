7146babd0406d2863702211d742958f7e235e092
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2023-11-20 15:56:39 -0300

    [feat] entender o router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListRenderComponent } from './components/list-render/list-render.component';
+import { ChangeNumberComponent } from './components/change-number/change-number.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListRenderComponent },
+  { path: 'change', component: ChangeNumberComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
